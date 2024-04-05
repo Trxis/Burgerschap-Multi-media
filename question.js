03be1c7bf282989c0ce91ad7f6631acfecfceae4
Trxis <t.chorabik@Sintlucasedu.nl>
2024-04-05 17:28:55 +0200

    Initial commit

---
+++ question.js
@@ -0,0 +1,64 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Binnen hoeveel jaar moet je bij niveau 3 en 4 een mbo-diploma halen om te zorgen dat het een gift wordt",
+        answer: "10 jaar",
+        options: [
+            "8 jaar",
+            "9 jaar",
+            "10 jaar",
+            "12 jaar"
+        
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Heb je een DigiD nodig om studiefinanciering aan te vragen?",
+        answer: "Ja",
+        options: [
+            "Ja",
+            "Nee",
+            "wat is een digiD?",
+            "PixelGun3d"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Vanaf welke leeftijd kun je voor een mbo-studie studiefinanciering aanvragen?",
+        answer: "18 jaar",
+        options: [
+            "21 jaar",
+            "16 jaar",
+            "18 jaar",
+            "20 jaar"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Hoeveel mag je maximaal bijverdienen naast je studiefinanciering in 2024?",
+        answer: "Maakt niet uit",
+        options: [
+            "Maakt niet uit",
+            "15.828,77 euro",
+            "7.500 euro",
+            "Je mag niet verdienen"
+
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Hoelang kun je maximaal studiefinanciering krijgen?",
+        answer: "7 jaar",
+        options: [
+            "4 jaar",
+            "10 jaar",
+            "7 jaar",
+            "8 jaar"
+
+        ]
+    },
+]
